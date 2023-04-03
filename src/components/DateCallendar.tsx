c1609ab1cbfbbe0f58b5d5be058dd7ffaee7a143
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-04-03 11:23:36 +0800

    connect absensi page with api

--- src/components/DateCallendar.tsx
+++ src/components/DateCallendar.tsx
@@ -1,4 +1,5 @@
 import { IonDatetime } from "@ionic/react";
+import moment from "moment";
 import { useState } from "react";
 
 interface IDateCallendar {
@@ -26,17 +27,19 @@ export default function DateCallendar({
   return (
     <>
       <input type="checkbox" checked={isOpen} className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
+      <div className="modal ">
+        <div className="modal-box w-[350px] overflow-hidden">
           <h3 className="text-xl font-semibold mb-6">Filter Tanggal</h3>
-          <div>
+          
             <IonDatetime
               locale="id-ID"
               presentation={presentation}
               preferWheel={true}
               onIonChange={(e) => setDate(e.detail.value)}
+              className="text-xs"
+              size="cover"
             />
-          </div>  
+          
 
           <div className="modal-action mt-0">
             <button className="btn btn-outline" onClick={handleCancel}>
@@ -44,7 +47,7 @@ export default function DateCallendar({
             </button>
 
             <button
-              className="btn btn-error"
+              className="btn bg-red-700"
               onClick={() => {
                 handleSubmit(date);
               }}
--- src/pages/Absensi.tsx
+++ src/pages/Absensi.tsx
@@ -1,43 +1,104 @@
 import { IonContent, IonPage } from "@ionic/react";
-import TitleHeader from "../components/TitleHeader";
+import moment from "moment";
+import { useEffect, useState } from "react";
 import { CiFilter } from "react-icons/ci/index";
+import DateCallendar from "../components/DateCallendar";
+import EmptyBox from "../components/EmptyBox";
+import Loading from "../components/Loading";
+import TitleHeader from "../components/TitleHeader";
+import { useGet } from "../hooks/useApi";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { AbsenEntity } from "../models/Absen.entity";
+import { GetPayload } from "../models/GenericPayload";
 const Absensi: React.FC = () => {
+  const [isFilterModal, setIsFilterModal] = useState(false);
+  const [date, setDate] = useState<string | string[] | null | undefined>("");
+  const [user] = useLocalStorage("user");
+
+  const {
+    data: payload,
+    isFetching,
+    refetch,
+  } = useGet<GetPayload<AbsenEntity>>({
+    name: "absens",
+    endpoint: `absens`,
+    filter: {
+      karyawan_id: user.karyawan.id,
+      date,
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [date]);
   return (
     <IonPage>
       <TitleHeader
         title="Absensi"
-        rightIcon={<CiFilter className="w-6 h-6" strokeWidth={1} />}
+        rightIcon={
+          <CiFilter
+            className="w-6 h-6 cursor-pointer"
+            strokeWidth={1}
+            onClick={() => setIsFilterModal(true)}
+          />
+        }
       />
-      
-      <IonContent fullscreen>
-        <div className="px-6 ">
-          <ul className="max-w-md divide-y-2 divide-black">
-            {[1, 2, 3, 1, 1, 1, 1, 1, 1, 1].map(() => (
-              <li className="py-3">
-                <div className="flex flex-col gap-3">
-                  <div className="flex-1 min-w-0">
-                    <p className="text-md font-bold text-gray-900 ">
-                      22 February 2022
-                    </p>
-                    <div>
-                      <div className="flex justify-between items-center">
-                        <p className="text-sm text-gray-500 ">In 08:00 - Out 17:00 </p>
-                        <p className="text-xs text-black font-black">Telat 0 Menit</p>
-                      </div>
 
-                      
-                    </div>
-                  </div>
-                  {/* <div className="text-xs self-end font-bold">
-                    Telat 0 Menit
-                  </div> */}
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <IonContent fullscreen>
+        {isFetching ? (
+          <Loading />
+        ) : (
+          <>
+            {payload?.data && payload.data.length > 0 ? (
+              <div className="px-6 ">
+                <ul className="max-w-md divide-y-2 divide-black">
+                  {payload?.data.map((absen) => (
+                    <li className="py-3">
+                      <div className="flex flex-col gap-3">
+                        <div className="flex-1 min-w-0">
+                          <p className="text-md font-bold text-gray-900 ">
+                            {moment(absen.tanggal).format("DD MMMM Y")}
+                          </p>
+                          <div>
+                            <div className="flex justify-between items-center">
+                              <p className="text-sm text-gray-500 ">
+                                In{" "}
+                                {absen.waktu_masuk.substring(
+                                  0,
+                                  absen.waktu_masuk.length - 3
+                                )}{" "}
+                                - Out{" "}
+                                {absen.waktu_keluar.substring(
+                                  0,
+                                  absen.waktu_keluar.length - 3
+                                )}{" "}
+                              </p>
+                              <p className="text-xs text-black font-black">
+                                Telat {absen.jumlah_telat} Menit
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <EmptyBox />
+            )}
+          </>
+        )}
       </IonContent>
-    
+      <DateCallendar
+        isOpen={isFilterModal}
+        handleCancel={() => setIsFilterModal(false)}
+        handleSubmit={(filterDate) => {
+          setDate(moment(filterDate).format("MMMM Y"));
+          setIsFilterModal(false);
+        }}
+        presentation={"month-year"}
+      />
     </IonPage>
   );
 };
